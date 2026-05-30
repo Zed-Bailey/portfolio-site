14c054e5991e5047d4a8872e7c873b86f41d6c7d
Zed-Bailey <emailzed03@gmail.com>
2026-05-30 17:10:02 +1000

    refactor: duplicated html, remove old routes

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,51 +1,358 @@
-import { Link } from "react-router";
-import type { Route } from "./+types/route";
-import { MenuLink } from "~/components/MenuLink/MenuLink";
-import {
-  GithubCircle,
-  LinkedInCircle,
-  MailCircle,
-} from "~/components/icons/Icons";
+import { useRef, useState } from "react";
 import { Card } from "~/components/Card/Card";
+import styles from "./Route.module.css";
+import classNames from "classnames/bind";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "Zoran Bailey's Portfolio" },
-    { name: "description", content: "Zoran Bailey - Software Engineer" },
-  ];
-}
+const cx = classNames.bind(styles);
 
-export default function Home() {
+export default function Home2() {
+  const [selected, setSelected] = useState<string>();
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const onClick = (s: string) => {
+    const isFirstSet = !selected;
+
+    setSelected((prev) => {
+      if (prev === s) return undefined;
+
+      return s;
+    });
+    const isDesktop = window.matchMedia("(min-width: 800px)").matches;
+
+    setTimeout(
+      () => {
+        const element = document.getElementById(`#${s}`);
+        element?.scrollIntoView({ behavior: "smooth" });
+      },
+      isFirstSet && isDesktop ? 300 : 0,
+    );
+  };
+
   return (
-    <section className="grid grid-cols-2 h-dvh">
-      <div className="sticky top-0 flex flex-col justify-between">
-        <div className="p-4">
-          <h3 className="text-3xl font-light text-slate-900">
-            Software Engineer
-          </h3>
-
-          <div className="flex gap-4 mt-6">
-            <a href="mailto:zoran.bailey@gmail.com">
-              <MailCircle size={64} />
-            </a>
-            <a href="https://github.com/Zed-Bailey">
-              <GithubCircle size={64} />
-            </a>
-            <a href="https://linkedin.com/in/zoran-bailey/">
-              <LinkedInCircle size={64} />
-            </a>
+    <div className="container mt-32">
+      <header className={cx("")}>
+        <section className={cx("headerGrid", "gap-20 md:px-6")}>
+          <div className="md:w-48 flex  md:flex-col items-center justify-between">
+            <div>
+              <h1 className="text-2xl font-bold">Zoran Bailey</h1>
+              <p>Software Engineer</p>
+            </div>
+            <svg
+              viewBox="0 0 480 480"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              width={200}
+              height={200}
+            >
+              <defs>
+                <clipPath id="blob">
+                  <path
+                    fill="#474bff"
+                    d="M441.5,292.5Q440,345,401,380.5Q362,416,315,442.5Q268,469,216.5,453Q165,437,124.5,407.5Q84,378,50.5,336.5Q17,295,25.5,242Q34,189,61.5,147.5Q89,106,126.5,73Q164,40,216,25Q268,10,320,29.5Q372,49,395.5,97.5Q419,146,431,193Q443,240,441.5,292.5Z"
+                  />
+                </clipPath>
+              </defs>
+              <image
+                x="0"
+                y="0"
+                width="100%"
+                height="100%"
+                clipPath="url(#blob)"
+                xlinkHref="princess.webp"
+                preserveAspectRatio="xMidYMid slice"
+              ></image>
+            </svg>
           </div>
+          <div>
+            <p>
+              Hello i'm Zoran! A software engineer from Melbourne, Australia!
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente
+              obcaecati sit ut illo accusantium consequatur, quos, laborum sint
+              recusandae atque nemo! Exercitationem inventore facere iste
+              tempora ea repudiandae, maxime praesentium?
+            </p>
+            <p className="my-2">
+              You can view my work on{" "}
+              <UnderLinedHoverLink
+                href="https://github.com/Zed-Bailey"
+                target="_blank"
+                label={<code>Github</code>}
+              />{" "}
+              or come say hi on{" "}
+              <UnderLinedHoverLink
+                label={"Linkedin"}
+                background="bg-[#0077B5]"
+                href="https://www.linkedin.com/in/zoran-bailey/"
+                target="_blank"
+              />
+            </p>
+
+            <p className="my-2">
+              Otherwise feel free to get in contact by shooting an email over to{" "}
+              <CopyEmailButton />
+            </p>
+          </div>
+        </section>
+        <hr className="my-10 col-span-3" />
+      </header>
+
+      <div
+        className={cx("menu", "items-start", { selected: Boolean(selected) })}
+      >
+        <div />
+        <nav className={cx(`flex flex-col sm:p-4 md:p-6`, "stickyHeader")}>
+          <HoverButton
+            onClick={() => onClick("work")}
+            data-selected={selected === "work"}
+            label={"WORK"}
+          />
+          <HoverButton
+            onClick={() => onClick("projects")}
+            data-selected={selected === "projects"}
+            label={"PROJECTS"}
+          />
+          <HoverButton
+            onClick={() => onClick("more")}
+            data-selected={selected === "more"}
+            label={"MORE"}
+          />
+        </nav>
+        <div
+          className={cx("content", "md:px-10 py-6", {
+            "visually-hidden-desktop": !selected,
+          })}
+          ref={containerRef}
+        >
+          <section id="#work" className={cx("contentSection")}>
+            <h2 className="text-4xl font-light">WORK</h2>
+            <p className="pt-2 pb-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+              quasi aliquid perferendis quo quis provident tempora. Repudiandae
+              possimus, optio deserunt asperiores, blanditiis nihil impedit ab
+              nam quod, consequuntur voluptatum natus!
+            </p>
+
+            <PositionInfo
+              logoUrl={"logo/tgg.svg"}
+              company={"The Good Guys"}
+              href={"https://thegoodguys.com.au"}
+              date={
+                <>
+                  2024{" "}
+                  <span className="text-sm text-green-500 font-bold">
+                    • Now
+                  </span>
+                </>
+              }
+              position={"Frontend Software Developer"}
+              description={"lorem"}
+              companyColour="#0055a5"
+            />
+            <PositionInfo
+              company={"Freelance"}
+              date={"2020 - 2021"}
+              description={"Lorem"}
+              position={"Freelance Web Developer"}
+            />
+
+            <h3 className="text-4xl font-light my-10">EDUCATION</h3>
+
+            <PositionInfo
+              logoUrl={"logo/rmit.svg"}
+              company={"RMIT University"}
+              position={"Bachelor of Information Technology"}
+              date={"2020 - 2024"}
+              href={"https://rmit.edu.au"}
+              subHeading={"Graduated with Distinction"}
+              logoWidth={64}
+              description={"lorem"}
+            />
+          </section>
+
+          <section id="#projects" className={cx("contentSection")}>
+            <h2 className="text-4xl font-light">
+              PROJECTS{" "}
+              <span className="text-sm">[ some of my favourites ]</span>
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+              quasi aliquid perferendis quo quis provident tempora. Repudiandae
+              possimus, optio deserunt asperiores, blanditiis nihil impedit ab
+              nam quod, consequuntur voluptatum natus!
+            </p>
+            <ProjectInfo />
+          </section>
+
+          <section id="#more" className={cx("contentSection")}>
+            <h2 className="text-4xl font-light">MORE</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+              quasi aliquid perferendis quo quis provident tempora. Repudiandae
+              possimus, optio deserunt asperiores, blanditiis nihil impedit ab
+              nam quod, consequuntur voluptatum natus!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+              quasi aliquid perferendis quo quis provident tempora. Repudiandae
+              possimus, optio deserunt asperiores, blanditiis nihil impedit ab
+              nam quod, consequuntur voluptatum natus!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+              quasi aliquid perferendis quo quis provident tempora. Repudiandae
+              possimus, optio deserunt asperiores, blanditiis nihil impedit ab
+              nam quod, consequuntur voluptatum natus!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+              quasi aliquid perferendis quo quis provident tempora. Repudiandae
+              possimus, optio deserunt asperiores, blanditiis nihil impedit ab
+              nam quod, consequuntur voluptatum natus!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+              quasi aliquid perferendis quo quis provident tempora. Repudiandae
+              possimus, optio deserunt asperiores, blanditiis nihil impedit ab
+              nam quod, consequuntur voluptatum natus!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+              quasi aliquid perferendis quo quis provident tempora. Repudiandae
+              possimus, optio deserunt asperiores, blanditiis nihil impedit ab
+              nam quod, consequuntur voluptatum natus!
+            </p>
+          </section>
         </div>
-        <h1 className="flex flex-col text-[200px]">
-          <span className="font-extrabold">ZORAN</span>
-          <span className="font-extralight">BAILEY</span>
-        </h1>
       </div>
-      <div className="grid grid-rows-3">
-        <MenuLink text="about" to="/about" />
-        <MenuLink text="projects" to="/projects" />
-        <MenuLink text="more" to="/more" />
-      </div>
-    </section>
+    </div>
   );
 }
+
+const HoverButton = ({ onClick, label, ...props }) => {
+  return (
+    <button
+      className={cx(
+        "hoverButton",
+        "w-full cursor-pointer font-light hover:bg-black hover:text-white p-4 sm:p-6 md:p-10 ",
+      )}
+      onClick={onClick}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
+
+const UnderLinedHoverLink = ({
+  label,
+  background = "bg-black",
+  textColourBase = "text-black",
+  textColourHover = "text-white",
+  ...props
+}: React.ComponentProps<"a"> & { label: string | React.ReactNode }) => {
+  return (
+    <a
+      {...props}
+      className="group relative px-2 py-1 overflow-hidden rounded-sm font-bold text-sm"
+    >
+      <span
+        className={`px-1 relative z-10 group-hover:${textColourHover} ${textColourHover} md:${textColourBase}`}
+      >
+        {label}
+      </span>
+      <span
+        className={`absolute left-0 bottom-0 w-full h-full md:h-0.5 transition-all ${background} z-0 group-hover:h-full`}
+      ></span>
+    </a>
+  );
+};
+
+const ProjectInfo = () => {
+  return (
+    <div className="my-10">
+      <h3 className="text-3xl font-light">
+        Project #1{" "}
+        <span className="text-sm">
+          [ a project to make everyone feel better ]
+        </span>
+      </h3>
+
+      <div className="mb-4 mt-2 flex gap-4 items-center">
+        <UnderLinedHoverLink label="Github" href="https://github.com" />
+        <img src="logo/typescript.svg" className="w-5 h-5 " />
+      </div>
+      <p>
+        This project Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Rem amet voluptatem odio, est at id velit cumque temporibus dolorum
+        blanditiis consectetur labore quas dicta, non, tempore necessitatibus
+        fuga mollitia tenetur?
+      </p>
+
+      <div className="mt-5">
+        <div className="w-full max-w-60 h-40 bg-black rounded-xl"></div>
+      </div>
+    </div>
+  );
+};
+
+const PositionInfo = ({
+  logoUrl,
+  logoWidth = 32,
+  href,
+  company,
+  companyColour = "black",
+  position,
+  date,
+  description,
+  subHeading,
+}) => {
+  const HeaderWrapper = href ? "a" : "span";
+
+  return (
+    <div className="flex mt-10 md:mt-20 flex-col">
+      <HeaderWrapper
+        className="hover:opacity-75 w-fit"
+        href={href ?? undefined}
+        target={href ? "_blank" : undefined}
+      >
+        {logoUrl ? (
+          <img src={logoUrl} width={logoWidth} className="rounded-sm" />
+        ) : null}
+        <span className={`text-[${companyColour}] font-semibold text-md`}>
+          {company}
+        </span>
+      </HeaderWrapper>
+
+      <span className="text-lg font-semibold flex sm:gap-4 sm:items-center sm:flex-row flex-col">
+        {position}{" "}
+        <span className="text-sm text-gray-500 font-bold">{date}</span>
+      </span>
+      {subHeading ? <span>{subHeading}</span> : null}
+      <p className="text-sm">{description}</p>
+    </div>
+  );
+};
+
+const CopyEmailButton = () => {
+  const [copied, setCopied] = useState(false);
+
+  const onClick = () => {
+    navigator.clipboard.writeText("zoranbaliey@gmail.com").finally(() => {
+      setCopied(true);
+      setTimeout(() => {
+        setCopied(false);
+      }, 3000);
+    });
+  };
+
+  return (
+    <button className={cx("copyButton", "mt-4")} onClick={onClick}>
+      <code className={cx("fore")}>
+        zoranbaliey@gmail.com{" "}
+        <span className={cx("copyText", "font-extralight", "text-[10px]")}>
+          [{copied ? "copied" : "copy"}]
+        </span>
+      </code>
+    </button>
+  );
+};
